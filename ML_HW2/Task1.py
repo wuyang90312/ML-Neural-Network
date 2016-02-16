9b847c8587f52e18546879dc625df081dd718e15
Chen  Xi <chenx265@ug173.eecg>
2016-02-15 20:35:08 -0500

    update Task1: add early stop mechanism (unverified)

--- ML_HW2/Task1.py
+++ ML_HW2/Task1.py
@@ -18,12 +18,12 @@ class SOFTMAX:
         b = tf.Variable(tf.zeros([10]), name="bias")
 
         # Set up the hyperparameters
-        learning_rate = 0.001
+        learning_rate = 0.001	
         momentum = 0.4
-        training_epochs = 10000
+        training_epochs = 1000
         
-        # start to train the model, maximize the log-likelihood
-        logits = tf.add(tf.matmul(X/255, W), b)
+        # start to train the model, maximize the log-likelihood / minimize the cost
+        logits = tf.add(tf.matmul(X, W), b)
         cost_batch = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y)
         cost = tf.reduce_mean(cost_batch)
         
@@ -32,60 +32,98 @@ class SOFTMAX:
         
         log_likelihood_train = -tf.reduce_mean(cost_batch)
         log_likelihood_valid = -tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=tf.add(tf.matmul(images_val/255, W), b), labels=labels_val))
+            logits=tf.add(tf.matmul(images_val, W), b), labels=labels_val))
 
         train_predition = tf.nn.softmax(logits)
-        valid_prediction = tf.nn.softmax(tf.add(tf.matmul(images_val/255, W), b))
-        test_prediction = tf.nn.softmax(tf.add(tf.matmul(images_test/255, W), b))
-
+        valid_prediction = tf.nn.softmax(tf.add(tf.matmul(images_val, W), b))
+        test_prediction = tf.nn.softmax(tf.add(tf.matmul(images_test, W), b))
 
         sess = tf.InteractiveSession()
         init = tf.initialize_all_variables()
         sess.run(init)
-        print("Initialized")
 
         result = np.zeros(shape=(5, training_epochs))
+        log_likelihood_max = -float("inf")
+        lilelihood_overfitting = False
+        lilelihood_oscillation = 0
+
+
+        print "leraning rate:" + repr(learning_rate)
+    	for epoch in range(training_epochs):
+            # Train the model
+	        for i in xrange(300):
+	            batch_xs = self.images[i * 50: (i + 1) * 50]
+	            batch_ys = self.labels[i * 50: (i + 1) * 50]
+	            cost_np, _ = sess.run([cost, train_op], feed_dict={X: batch_xs, Y: batch_ys})
+            
+	        likelihood_train, predictions_train = sess.run([log_likelihood_train, train_predition], 
+	        	feed_dict={X: self.images, Y:self.labels})
+	        accuracy_train = self.accuracy(predictions_train, self.labels)
+	        likelihood_val = log_likelihood_valid.eval()
+	        accuracy_val = self.accuracy(valid_prediction.eval(), labels_val)
+
+	        result[0, epoch] = epoch + 1
+	        result[1, epoch] = (100 - accuracy_val) * 1000 / 100
+	        result[2, epoch] = (100 - accuracy_train) * 15000 / 100
+	        result[3, epoch] = likelihood_train
+	        result[4, epoch] = likelihood_val
+	   
+	        if(epoch % 10 == 0):
+	        	print ("Epoch:%04d, Train Accuracy=%0.4f, Eval Accuracy=%0.4f, log_likelyhood_train:%0.6f, log_likelyhood_val:%0.6f" %
+	        		(epoch+1, accuracy_train, accuracy_val, likelihood_train, likelihood_val))
+
+	        if(log_likelihood_max < likelihood_val):
+	        	log_likelihood_max = likelihood_val
+	        	lilelihood_oscillation = 0
+	        elif(log_likelihood_max - likelihood_val > 0):
+	        	lilelihood_oscillation += 1
+
+	        if(lilelihood_overfitting == False):
+	        	if(lilelihood_oscillation >= 15):
+	        	    lilelihood_overfitting = True
+	        	    print "Likelihood overfitting"
+	        	    print ("Epoch:%04d, Train Accuracy=%0.4f, Eval Accuracy=%0.4f, log_likelyhood_train:%0.6f, log_likelyhood_val:%0.6f" % 
+		               (epoch+1, accuracy_train, accuracy_val, likelihood_train, likelihood_val))
+	        	    accuracy_test = self.accuracy(test_prediction.eval(), labels_test)
+	        	    print ("Test Accuracy=%0.4f" % (accuracy_test))
+	        	    print 'Test_Error: ' + repr((100 - accuracy_test) * 2720 / 100)
+
+	        	    plt.plot(result[0,:epoch], result[1,:epoch],'b-', label='Validation Error')
+	        	    plt.plot(result[0,:epoch], result[2,:epoch],'r-', label = 'Training Error')
+	        	    plt.legend(loc = 'upper right', numpoints = 1)
+	        	    plt.xlabel('Number of Epochs')
+	        	    plt.ylabel('Number of Errors')
+	        	    plt.savefig('err_early')
+	        	    plt.close()
+                    	        	    
+	        	    plt.plot(result[0,:epoch], result[3,:epoch],'b-', label = 'Log-likelihood_Training')
+	        	    plt.plot(result[0,:epoch], result[4,:epoch],'r-', label = 'Log-likelihood_Validation')
+	        	    plt.legend(loc = 'lower right', numpoints = 1)
+	        	    plt.xlabel('Number of Epochs')
+	        	    plt.ylabel('Log-likelihood')
+	        	    plt.savefig('log_early')
+	        	    plt.close()
         
-        for epoch in range(training_epochs):
-            for i in xrange(150):
-                batch_xs = self.images[i * 100: (i + 1) * 100]
-                batch_ys = self.labels[i * 100: (i + 1) * 100]
-                cost_np, _ = sess.run([cost, train_op], feed_dict={X: batch_xs, Y: batch_ys})
-
-            likelihood_train, predictions_train = sess.run([log_likelihood_train, train_predition], 
-            	feed_dict={X: self.images, Y:self.labels})
-            accuracy_train = self.accuracy(predictions_train, self.labels)
-            likelihood_val = log_likelihood_valid.eval()
-            accuracy_val = self.accuracy(valid_prediction.eval(), labels_val)
-
-            result[0, epoch] = epoch + 1
-            result[1, epoch] = (100 - accuracy_val) * 1000 / 100
-            result[2, epoch] = (100 - accuracy_train) * 15000 / 100
-            result[3, epoch] = likelihood_train
-            result[4, epoch] = likelihood_val
-
-            if(epoch % 10 == 0):
-            	print ("Epoch:%04d, Train Accuracy=%0.4f, Eval Accuracy=%0.4f, log_likelyhood_train:%0.6f, log_likelyhood_val:%0.6f" %
-            		(epoch+1, accuracy_train, accuracy_val, likelihood_train, likelihood_val))
-
-        #Predict Test Error
+        print "Complete:"
         accuracy_test = self.accuracy(test_prediction.eval(), labels_test)
         print ("Test Accuracy=%0.4f" % (accuracy_test))
         print 'Test_Error: ' + repr((100 - accuracy_test) * 2720 / 100)
 
+    	plt.plot(result[0,:], result[1,:],'b-', label='Validation Error')
+    	plt.plot(result[0,:], result[2,:],'r-', label = 'Training Error')
+    	plt.legend(loc = 'upper right', numpoints = 1)
+    	plt.xlabel('Number of Epochs')
+    	plt.ylabel('Number of Errors')
+        plt.savefig('err')
+        plt.close()
 
-        plt.plot(result[0,:], result[1,:],'b-', label='Validation Error')
-        plt.plot(result[0,:], result[2,:],'r-', label = 'Training Error')
-        plt.legend(loc = 'upper right', numpoints = 1)
-        plt.xlabel('Number of Epochs')
-        plt.ylabel('Number of Errors')
-        plt.show()
-        plt.plot(result[0,:], result[3,:],'b-', label = 'Log-likelihood_Training')
-        plt.plot(result[0,:], result[4,:],'r-', label = 'Log-likelihood_Validation')
-        plt.legend(loc = 'lower right', numpoints = 1)
-        plt.xlabel('Number of Epochs')
-        plt.ylabel('Log-likelihood')
-        plt.show()
+    	plt.plot(result[0,:], result[3,:],'b-', label = 'Log-likelihood_Training')
+    	plt.plot(result[0,:], result[4,:],'r-', label = 'Log-likelihood_Validation')
+    	plt.legend(loc = 'lower right', numpoints = 1)
+    	plt.xlabel('Number of Epochs')
+    	plt.ylabel('Log-likelihood')
+        plt.savefig('log')
+        plt.close()
 
 
 with np.load("notMNIST.npz") as data:
@@ -98,5 +136,5 @@ for i in labels:
     labels_in[index, i] = 1
     index +=1
 
-softmax = SOFTMAX(images_in[:15000,:], labels_in[:15000,:])
-softmax.constru_SOFTMAX(images_in[15000:16000,:], labels_in[15000:16000,:], images_in[16000:18720,:], labels_in[16000:18720,:])
+softmax = SOFTMAX(images_in[:15000,:]/255, labels_in[:15000,:])
+softmax.constru_SOFTMAX(images_in[15000:16000,:]/255, labels_in[15000:16000,:], images_in[16000:18720,:]/255, labels_in[16000:18720,:])
